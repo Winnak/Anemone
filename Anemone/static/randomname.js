3ea1f54b054296a20e8030f00a4f5328599e7555
Erik Hoyjor <ErikHoyJor@gmail.com>
2016-05-03 12:36:53 +0200

    fixed issue in random name generation, moved name data into namegen.html

--- Anemone/static/randomname.js
+++ Anemone/static/randomname.js
@@ -1,9 +1,9 @@
-function getRandomName(elementID) {
+function getRandomName(elementID) { // TODO: request namegen data once, instead every time.
     $.ajax({
         url : "/namegen",
         success : function(result){
             var data = result.split("\n");
-            var selection = data[Math.floor(Math.random() * data.length - 1)];
+            var selection = data[Math.floor(Math.random() * data.length)];
             selection += data[Math.floor(Math.random() * data.length)];
             document.getElementById(elementID).value = selection;
         }
