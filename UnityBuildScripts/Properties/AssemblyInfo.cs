3e31aba79cd633a99864a85c041ecd6e67550173
Erik Hoyjor <ErikHoyJor@gmail.com>
2016-07-09 01:03:14 +0200

    updated anemone dlls

--- UnityBuildScripts/Properties/AssemblyInfo.cs
+++ UnityBuildScripts/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
@@ -16,14 +15,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Anemone")]
 [assembly: AssemblyCopyright("MIT - Erik Høyrup Jørgensen - 2016")]
 [assembly: AssemblyTrademark("Anemone")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("06d88e1e-9068-41f1-a2b4-85c6fe940662")]
-
 [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyFileVersion("1.0.*")]
